1866e0a188c5d21270af85b8f56a1dee89046d85
Dior K <qw5057@naver.com>
2022-11-16 20:23:57 +0900

    WPFCommandsDemo3 추가

---
+++ WPFCommandsDemo2/ViewModels/MessageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WPFCommandsDemo2.Models;
+
+namespace WPFCommandsDemo2.ViewModels
+{
+    public class MessageViewModel
+    {
+        //public string MessageText { get; set; }
+        public MessageCommand DisplayMessageCommand { get; private set; }
+        public MessageViewModel()
+        {
+            DisplayMessageCommand = new MessageCommand(DisplayMessage);
+        }
+        public void DisplayMessage(string message)
+        {
+            //MessageBox.Show(MessageText);
+            MessageBox.Show(message);
+        }
+    }
+}
